6c0551c59e335f7c0719570d4c35efd48d9403f3
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-05-16 18:24:16 +0000

    sdfsdfsdf

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+function updateCookieCount() {
+  if (localStorage.hits) {
+  localStorage.hits = Number(localStorage.hits) + 1;
+  }
+  else {
+    localStorage.hits = 1
+  }
+  document.getElementById("times").innerHTML = "Total click is:" + localStorage.hits
+}
